7fdb10d5d76623ae4416fc594e23a0ebab841fd8
shaw <gggshaw007@gmail.com>
2019-01-23 15:11:18 +0800

    make a jQuery

--- src/index.js
+++ src/index.js
@@ -1,35 +1,50 @@
-class Person {
-  constructor(name, age) {
-    this.name = name
-    this.age = age
+class jQuery {
+  constructor(selector) {
+    let slice = Array.prototype.slice
+    //将DOM节点转换为数组
+    let dom = slice.call(document.querySelectorAll(selector))
+    let len = dom ? dom.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
   }
-  eat() {
-    alert(`${this.name} eat something`)
-  }
-  speak() {
-    alert(`My name is ${this.name}, age ${this.age}`)
-  }
-  getName() {
-    return this.name
-  }
-  saySomething() {}
+  append(node) {}
+  addClass(name) {}
+  html(data) {}
 }
 
-class Student extends Person {
-  constructor(name, age, number) {
-    super(name, age)
-    this.number = number
-  }
-  study() {
-    alert(`${this.name} study`)
-  }
-  saySomething() {
-    alert('I am student')
-  }
+window.$ = function(selector) {
+  // 工厂模式
+  return new jQuery(selector)
 }
 
-let xiaoming = new Student('xiaoming', 10, 'A1')
-xiaoming.study()
-console.log(xiaoming.number)
-xiaoming.eat()
-xiaoming.saySomething()
+
+function loadImg(src) {
+  var promise = new Promise(function (resolve, reject) {
+    var img = document.createElement('img')
+    img.onload = function () {
+      resolve(img)
+    }
+    img.onerror = function () {
+      reject('load error')
+    }
+    img.src = src
+  })
+  return promise
+}
+
+var src = 'https://www.imooc.com/static/img/index/logo_new.png'
+var result = loadImg(src)
+
+result.then(function (img) {
+  console.log('img,width', img.width)
+  return img
+}).then(function (img) {
+  console.log('img.height', img.height)
+}).catch(function (ex) {
+  console.log(ex)
+})
+// 单一职责原则，每个then中只作好一件事
+// 开放封闭原则，如果新增需求，扩展then -- 对扩展开放，对修改封闭
